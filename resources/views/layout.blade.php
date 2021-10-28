169de7364e3dc0ca10015e8453d95ab102407010
Mehrab137 <mehrabarnob98@gmail.com>
2021-10-28 16:48:09 -0700

    changed basic table to jquery datatable

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,40 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="{{ url('/') }}">LaptopStore</a>
-          
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+            <a class="navbar-brand" href="{{ url('/') }}">LaptopStore</a>
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-          <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse" id="navbarNav">
 
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Home</a>
-              </li>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Home</a>
+                    </li>
 
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('add.laptop.view') }}">Add Laptops</a>
-              </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('add.laptop.view') }}">Add Laptops</a>
+                    </li>
 
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('view.laptop.list') }}">View Laptops</a>
-              </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('view.laptop.list') }}">View Laptops</a>
+                    </li>
 
-            </ul>
-          </div>
+                </ul>
+            </div>
         </div>
-      </nav>
+    </nav>
 
-      <div class="container">
-          @yield('content')
-      </div>
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+
+    @stack('js')
 
 </body>
 </html>
--- resources/views/view_laptop.blade.php
+++ resources/views/view_laptop.blade.php
@@ -1,42 +1,52 @@
 @extends('layout')
 
 @section('content')
-    
+
+<p style="font-size: x-large"><b>Here are the informations recorded, organised in a Datatable</b></p>
 <div class="row">
-    <div class="col">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>Brand Name</th>
-                    <th>Model Name</th>
-                    <th>Processor</th>
-                    <th>Number of Cores</th>
-                    <th>RAM</th>
-                </tr>
-            </thead>
-            <tbody>
-
-                @foreach ($laptops as $laptop)
+    <div class="col-md-12">
+        <div class="table-responsive">
+            <table id="laptops_table" class="table table-bordered table-striped table-hover">
+                <thead>
                     <tr>
-                        <td>{{ $laptop->id }}</td>
-                        <td>{{ $laptop->brand  }}</td>
-                        <td>{{ $laptop->model  }}</td>
-                        <td>{{ $laptop->processor }}</td>
-                        <td>{{ $laptop->cores }}</td>
-                        <td>{{ $laptop->ram }}</td>
+                        <th>id</th>
+                        <th>Brand Name</th>
+                        <th>Model Name</th>
+                        <th>Processor</th>
+                        <th>Number of Cores</th>
+                        <th>RAM</th>
                     </tr>
+                </thead>
 
-                @endforeach
+                <tbody>
 
-            </tbody>
-        </table>
+                    @foreach ($laptops as $laptop)
+                        <tr>
+                            <td>{{ $laptop->id }}</td>
+                            <td>{{ $laptop->brand }}</td>
+                            <td>{{ $laptop->model }}</td>
+                            <td>{{ $laptop->processor }}</td>
+                            <td>{{ $laptop->cores }}</td>
+                            <td>{{ $laptop->ram }}</td>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 
-
-
-
-
-
 @endsection
+
+@push('js')
+    
+<script>
+
+    $(document).ready( function () {
+        $('#laptops_table').DataTable();
+    });
+
+</script>
+
+@endpush
